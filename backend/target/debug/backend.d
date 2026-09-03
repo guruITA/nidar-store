ed40854aaff34ba56174b0bb52e264776204b69a
Gurpreet Singh <Singhgurpreet14082002@gmail.com>
2026-09-03 17:22:58 +0200

    chore(backend): remove unused backend files and configurations

--- backend/target/debug/backend.d
+++
@@ -1,1 +0,0 @@
-C:\nidar-store\backend\target\debug\backend.exe: C:\nidar-store\backend\src\main.rs
--- backend/target/debug/deps/backend.d
+++
@@ -1,5 +0,0 @@
-C:\nidar-store\backend\target\debug\deps\backend.d: src\main.rs
-
-C:\nidar-store\backend\target\debug\deps\backend.exe: src\main.rs
-
-src\main.rs:
