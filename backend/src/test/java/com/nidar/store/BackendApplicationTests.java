d4ef04c0aa3f15e72d26c687953fc35751ce6f56
Gurpreet Singh <Singhgurpreet14082002@gmail.com>
2026-09-03 18:01:54 +0200

    chore(backend): add initial project structure with Maven configuration and Spring Boot setup

---
+++ backend/src/test/java/com/nidar/store/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nidar.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
