d40cc1e4eba13fdf4299a2d6df6c205eb4a6f2af
Gurpreet Singh <Singhgurpreet14082002@gmail.com>
2026-09-03 15:56:12 +0200

    chore(backend): initialize Rust project

---
+++ backend/target/debug/backend.d
@@ -0,0 +1,1 @@
+C:\nidar-store\backend\target\debug\backend.exe: C:\nidar-store\backend\src\main.rs
---
+++ backend/target/debug/deps/backend.d
@@ -0,0 +1,5 @@
+C:\nidar-store\backend\target\debug\deps\backend.d: src\main.rs
+
+C:\nidar-store\backend\target\debug\deps\backend.exe: src\main.rs
+
+src\main.rs:
